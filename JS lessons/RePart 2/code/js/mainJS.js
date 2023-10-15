82df9292d6ece006c88b77e1850c64ff6fc165cc
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-10-15 21:19:03 +0300

    RePart 2 (3)
    DOM navigation.

--- JS lessons/RePart 2/code/js/mainJS.js
+++ JS lessons/RePart 2/code/js/mainJS.js
@@ -6,3 +6,29 @@ console.log(location)
 //alert
 //alert('Hello!')
 //DOM
+
+let body = document.body
+let doc = document
+
+console.log(body)
+console.log(doc)
+
+let bodyText = document.body.firstChild
+
+console.log(bodyText)
+console.log(body.childNodes)
+console.log(body.hasChildNodes())
+
+let bodyChild = body.childNodes
+
+console.log(body.childNodes.length)
+
+for (let i = 0; i < bodyChild.length; i++) {
+    console.log(bodyChild[i])
+}
+
+console.log(body.parentNode)
+
+console.log(body.children)
+
+console.log(body.firstElementChild)
