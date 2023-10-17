03ebea165f6fbed4383d365b2bfe49152d579190
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-10-17 19:49:08 +0300

    RePart 2 (5)
    RePart 2 (5).Finish RePart 2.

--- JS lessons/RePart 2/code/js/mainJS.js
+++ JS lessons/RePart 2/code/js/mainJS.js
@@ -58,3 +58,74 @@ console.log(
 console.log(
     document.getElementById('text')
 )
+
+console.log(
+    text.innerHTML
+)
+text.innerHTML = 'Life and job after that!'
+
+let text2 = document.querySelector('.text2')
+text2.outerHTML = `
+<p id="text3" class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, accusamus.</p>
+`
+
+const newElement = document.createElement('div')
+
+newElement.innerHTML = `
+<p class='text' id='2'>Hi there!</p>
+`
+document.querySelectorAll('.text')[1].after(newElement)
+
+
+let div1 = document.getElementById('div')
+
+div1.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <p class='text' id='3'>Hi there v2!</p>
+    `
+)
+
+let wrapper = document.querySelector('.wrapper')
+
+wrapper.append(
+    titleMassif[0],
+    text,
+    text3,
+    document.getElementById('2'),
+    document.getElementById('3'),
+)
+
+let title2 = titleMassif[0].cloneNode(true)
+
+title2.innerHTML =
+    `
+    My name is Alex!
+    `
+
+wrapper.append(
+    title2
+)
+
+console.log(text3.className)
+text3.classList.add('yellow')
+
+num = 1
+
+for (let classs of text3.classList) {
+    console.log(num)
+    num++
+    console.log(classs)
+}
+
+title2.style.color = 'blue'
+title2.style.paddingTop = '20px'
+
+console.log(title2.style.paddingTop)
+
+console.log(getComputedStyle(title2))
+console.log(getComputedStyle(title2).fontSize)
+
+let h2PX = parseInt(getComputedStyle(title2).fontSize) // parseInt = only numbers
+
+console.log(h2PX)
